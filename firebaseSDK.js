94e018317c8e0abf475943a0175e59dee869c548
duy08k4 <duytran.290804@gmail.com>
2025-01-07 19:16:27 +0700

    Update

--- firebaseSDK.js
+++ firebaseSDK.js
@@ -1,7 +1,8 @@
 const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
 const { getFirestore, Timestamp, FieldValue, Filter } = require('firebase-admin/firestore');
 
-const serviceAccount = require(process.env.FIREBASE_API_KEY);
+// const serviceAccount = require(process.env.FIREBASE_API_KEY);
+const serviceAccount = JSON.parse(process.env.FIREBASE_API_KEY);
 
 initializeApp({
   credential: cert(serviceAccount)
